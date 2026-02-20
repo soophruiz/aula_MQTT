9b4f8f6168bd8b87eb2746b26a52ce08527cf5fe
Sophia <sophiaruiztoledo@gmail.com>
2026-02-20 13:50:00 -0300

    Add files via upload
    Projeto teste MQTT com publisher e subscriber

---
+++ subscriber.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+
+broker = "broker.hivemq.com"
+port = 1883
+topic = "senai/dev"
+
+def on_message(client, userdata, msg):
+    print(f"Mensagem recebida no tópico '{msg.topic}': {msg.payload.decode()}")
+
+client=mqtt.Client()
+client.on_message = on_message
+client.connect(broker, port)
+
+client.subscribe(topic)
+print(f"Inscrito no tópico {topic}', aguardando mensagens...")
+
+client.loop_forever()
